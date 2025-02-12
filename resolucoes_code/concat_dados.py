0edc669a8a63b81a4691032417544ecc76d046cf
Nikolas <116127550+Nikao1@users.noreply.github.com>
2025-02-12 14:53:10 +0000

    resolvendo desafios de codigo no codespace

--- resolucoes_code/concat_dados.py
+++ resolucoes_code/concat_dados.py
@@ -1,1 +1,8 @@
 # Vamos receber dois dados diferentes do usuário e concatena-los em uma única string?!
+
+dado1 = input("Digite o nome do seu cachorro: ")
+dado2 = input("Digite um adjetivo que combina com ele: ")
+
+cachorro_eh = dado1 + (" é ") + dado2
+
+print (f"meu cachorro {cachorro_eh}")
--- resolucoes_code/ope_mat.py
+++ resolucoes_code/ope_mat.py
@@ -1,1 +1,8 @@
 # Vamos solicitar como entrada dois números e depois vamos realizar uma operação simples entre eles.
+
+numero1 = float(input("Insira um número: "))
+numero2 = float(input("Insira outro número: "))
+
+soma = numero1 + numero2
+
+print(soma)
